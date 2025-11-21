8dfabe9b054a347dc29bfffc679d0d341e6b2672
JagHatarElgiganten123 <d25antli@student.his.se>
2025-11-21 10:54:33 +0100

    Time Challange

--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -1,22 +1,28 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
-    private float timer;
+    private int timer;
     [SerializeField] private TextMeshProUGUI timerGUI;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        timer = 10;
+        timer = 600;
         timerGUI.text = timer.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer = timer - Time.deltaTime;
+        timer = timer - 1;
         timerGUI.text = timer.ToString();
+
+        if (timer <= 0)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
     }
 }
